368b108cef14d59e290c980628562ce280d1fd13
drumsjuanma <joan.arcas.sanz@accenture.com>
2018-01-10 17:47:39 +0100

    new Smart Contract "Token.sol"

---
+++ Token.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.19;
+
+contract Token {
+
+  // Mapeo de balances
+  mapping(address => uint256) balances;
+
+  // Total Supply
+  uint256 totalSupply;
+
+  // Propietario del Smart Contract
+  address public owner;
+  
+  function Token(uint256 _totalSupply) public {
+      totalSupply = _totalSupply;
+  }
+ 
+  // Funcion para comprovar el numero de tokens de una cuenta
+  function balanceOf(address _owner) public constant returns (uint256 balance) {
+    return balances[_owner];
+  }
+  
+  // Transfiere tokens entre dos cuentas
+  function transfer(address _to, uint256 _amount) public returns (bool success) {
+    if (balances[msg.sender] >= _amount && _amount > 0 && balances[_to] + _amount > balances[_to]) {
+      balances[msg.sender] -= _amount;
+      balances[_to] += _amount;
+        return true;
+      } else {
+        return false;
+      }
+   }
+
+
+}
